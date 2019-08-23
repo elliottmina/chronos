55df7c56a31e0e6af31c05187d2576885588b341
Elliott Mina <emina@fionta.com>
2019-08-23 10:20:18 -0700

    Visual fixes WIP.

--- docroot/modules/Goal/GoalTemplate.js
+++ docroot/modules/Goal/GoalTemplate.js
@@ -1,14 +1,14 @@
 var GoalTemplate = `
 <section class="doughnut" id="GoalToday">
   <label>Day</label>
-  <canvas id="GoalTodayChart" height="200" width="400"></canvas>
+  <canvas id="GoalTodayChart" height="150" width="300"></canvas>
   <span class="hour_value"></span>
   <span class="percent_value"></span>
   <span class="hours_remaining"></span>
 </section>
 <section class="doughnut" id="GoalWeekly">
   <label>Week</label>
-  <canvas id="GoalWeeklyChart" height="200" width="400"></canvas>
+  <canvas id="GoalWeeklyChart" height="150" width="300"></canvas>
   <span class="hour_value"></span>
   <span class="percent_value"></span>
   <span class="hours_remaining"></span>
--- docroot/modules/Stats/StatsTemplate.js
+++ docroot/modules/Stats/StatsTemplate.js
@@ -1,17 +1,17 @@
 var StatsTemplate = `
 <section class="pie" id="StatsEfficiency">
-  <canvas id="StatsEfficiencyChart" height="200" width="400"></canvas>
+  <canvas id="StatsEfficiencyChart" height="150" width="300"></canvas>
 </section>
 <section class="pie" id="StatsToday">
-  <canvas id="StatsTodayChart" height="200" width="400"></canvas>
+  <canvas id="StatsTodayChart" height="150" width="300"></canvas>
 </section>
 <section class="pie" id="StatsTodayRoot">
-  <canvas id="StatsTodayRootChart" height="200" width="400"></canvas>
+  <canvas id="StatsTodayRootChart" height="150" width="300"></canvas>
 </section>
 <section class="pie" id="StatsWeekly">
-  <canvas id="StatsWeeklyChart" height="200" width="400"></canvas>
+  <canvas id="StatsWeeklyChart" height="150" width="300"></canvas>
 </section>
 <section class="pie" id="StatsWeeklyRoot">
-  <canvas id="StatsWeeklyRootChart" height="200" width="400"></canvas>
+  <canvas id="StatsWeeklyRootChart" height="150" width="300"></canvas>
 </section>
 `;
