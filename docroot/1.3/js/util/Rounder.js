aee08702ec4bd161433fc228f19fbbe3fe946a66
Elliott Mina <elliottmina@gmail.com>
2019-08-30 19:04:20 -0700

    Removing beta testing folder.

--- docroot/1.3/js/util/Rounder.js
+++
@@ -1,6 +0,0 @@
-var Rounder = {
-  roundDecimal:function(num, orders) {
-    var multiplier = Math.pow(10, orders);
-    return Math.trunc(num*multiplier)/multiplier;
-  }
-};
--- docroot/1.3/modules/SpanSummary/SpanSummaryTemplate.js
+++
@@ -1,7 +0,0 @@
-var SpanSummaryTemplate = `
-  <header>Spans</header>
-  <div class="no_content_container">Nothing to see here.  Move along.</div>
-  <div class="content_container">
-    <input type="text" placeholder="filter" class="filter" />
-    <ul class="item_container"></ul>
-  </div>`;
