f9963d9a32781b885aa8f15029dfc8574db10cfd
Elliott Mina <elliottmina@gmail.com>
2022-01-29 16:15:55 -0500

    Removing span filter.

--- docroot/modules/SpanSummary/SpanSummaryTemplate.js
+++ docroot/modules/SpanSummary/SpanSummaryTemplate.js
@@ -2,6 +2,5 @@ var SpanSummaryTemplate = `
   <header>Spans</header>
   <div class="no_content_container">Nothing to see here.  Move along.</div>
   <div class="content_container">
-    <input type="text" placeholder="filter" class="filter" />
     <ul class="item_container"></ul>
   </div>`;
