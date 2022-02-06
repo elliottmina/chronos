c0bce781a8b793646384c0075118b9772553e050
Elliott Mina <elliottmina@gmail.com>
2022-02-06 16:58:57 -0500

    WIP style updates.

--- docroot/modules/Goal/GoalTemplate.js
+++ docroot/modules/Goal/GoalTemplate.js
@@ -2,14 +2,14 @@ var GoalTemplate = `
 <h2>Progress</h2>
 <section id="goal-daily">
   <progress-bar><inner-bar></inner-bar></progress-bar>
-  <label>Daily</label>
   <span class="hour_value text"></span>
   <span class="hours_remaining"></span>
+  <label>Daily</label>
 </section>
 <section id="goal-weekly">
   <progress-bar><inner-bar></inner-bar></progress-bar>
-  <label>Weekly</label>
   <span class="hour_value text"></span>
   <span class="hours_remaining"></span>
+  <label>Weekly</label>
 </section>
 `;
