fe1b870396d8be9720812eb662ad87d1478e5faa
Elliott Mina <elliottmina@gmail.com>
2022-02-06 22:16:53 -0500

    Style changes.

--- docroot/modules/SpanSummary/SpanSummaryTemplate.js
+++ docroot/modules/SpanSummary/SpanSummaryTemplate.js
@@ -1,5 +1,5 @@
 var SpanSummaryTemplate = `
-  <header>Spans</header>
+  <header>Log</header>
   <div class="no_content_container">Nothing to see here.  Move along.</div>
   <div class="content_container">
     <ul class="item_container"></ul>
