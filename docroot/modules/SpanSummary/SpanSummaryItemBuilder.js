a554d8bdb2ffdb003fa27d3bf2996e4e43bd424a
Elliott Mina <elliottmina@gmail.com>
2022-02-06 22:52:13 -0500

    Style changes.

--- docroot/modules/SpanSummary/SpanSummaryItemBuilder.js
+++ docroot/modules/SpanSummary/SpanSummaryItemBuilder.js
@@ -19,7 +19,7 @@ var SpanSummaryItemBuilder = function(
     var colorTrans = App.colorGenerator.generate(projectRoot, 0.3);
     var color = App.colorGenerator.generate(projectRoot);
     const label = container.find('label');
-    container.css('border-color', App.colorGenerator.generate(projectRoot, 0.5));
+    container.css('border-color', App.colorGenerator.generate(projectRoot, 0.8));
     container.css('background-color', App.colorGenerator.generate(projectRoot, 0.2));
   };
 
