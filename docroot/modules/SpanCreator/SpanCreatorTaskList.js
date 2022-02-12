61cd9c8ea0013e27a1d7f8c2b64b11ff3c777b01
Elliott Mina <elliottmina@gmail.com>
2022-02-12 13:46:17 -0500

    Extracting task suggestor from task list, WIP

--- docroot/modules/SpanCreator/SpanCreatorTaskList.js
+++ docroot/modules/SpanCreator/SpanCreatorTaskList.js
@@ -3,8 +3,6 @@ var SpanCreatorTaskList = function(topContainer) {
   var input;
   var assignedList;
   var assignedTasks = [];
-  var spans;
-  var availableList;
 
   var init = function() {
     gatherComponents();
@@ -14,15 +12,11 @@ var SpanCreatorTaskList = function(topContainer) {
   var gatherComponents = function() {
     input = topContainer.find('input');
     assignedList = topContainer.find('ul.assigned');
-    availableList = topContainer.find('ul.available');
   };
 
   var addBehavior = function() {
     input.keyup(onKeyUp);
     new InputSizeAdjuster(input);
-    App.dispatcher.subscribe('DATE_CHANGED', onDateChanged);
-    App.dispatcher.subscribe('SPAN_SAVED', onSpanSaved);
-    App.dispatcher.subscribe('SPAN_DELETED', onSpanDeleted);
   };
 
   var onKeyUp = function(e) {
@@ -58,7 +52,6 @@ var SpanCreatorTaskList = function(topContainer) {
     var taskText = anchor[0].nextSibling.textContent;
     assignedTasks.splice(assignedTasks.indexOf(taskText), 1);
     anchor.parent().remove();
-    updateSpansAvailable();
     publishChange();
   };
 
@@ -67,62 +60,10 @@ var SpanCreatorTaskList = function(topContainer) {
     assignedList.empty();
     input.val('');
     publishChange();
-    updateSpansAvailable();
   };
 
   var publishChange = function() {
     App.dispatcher.publish('SPAN_CHANGED');
-  };
-
-  var onDateChanged = function(date) {
-    spans = date.spans;
-    updateSpansAvailable();
-  };
-
-  var onSpanSaved = function(data) {
-    spans = data.record.spans;
-    updateSpansAvailable();
-  };
-
-  var onSpanDeleted = function(data) {
-    spans = data.record.spans;
-    updateSpansAvailable();
-  };
-
-  var updateSpansAvailable = function() {
-    availableList.empty();
-
-    if (spans === undefined)
-      return;
-
-    buildUnassignedTasks(spans).forEach(task => {
-      const item = jQuery('<li><i class="far fa-plus"></i></li>');
-      item.append(task);
-      item.click(addAvailableTask);
-      availableList.append(item);
-    });
-  };
-
-  var buildUnassignedTasks = function() {
-    var allTasks = [];
-    Object.entries(spans).forEach(span => {
-      allTasks = [...allTasks, ...span[1].tasks];
-    });
-
-    const uniqueTasks = allTasks.filter((task, index) => {
-      return allTasks.indexOf(task) === index;
-    });
-
-    const unassignedTasks = uniqueTasks.filter((task, index) => {
-      return assignedTasks.indexOf(task) === -1;
-    });
-
-    return unassignedTasks;
-  };
-
-  var addAvailableTask = function() {
-    addTask(this.innerText);
-    updateSpansAvailable();
   };
 
   init();
