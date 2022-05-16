71c33f4baceeb5fd3903e56f3ec879c380b2936c
Elliott Mina <elliottmina@gmail.com>
2022-05-16 08:58:18 -0400

    Updates to day summary.

--- docroot/modules/DaySummary/DaySummary.js
+++ docroot/modules/DaySummary/DaySummary.js
@@ -1,48 +1,40 @@
 var DaySummary = function() {
 
-  var html = `
-    <table class="summary">
-      <tbody></tbody>
-    </table>`;
-
-  var dataBuilder;
-  var copier;
-  var tbody;
+  var container;
   var spans;
+  var dataBuilder;
   var itemBuilder;
   var totalsBuilder;
 
   var init = function() {
-    dataBuilder = new ProjectSummaryDataBuilder();
-    copier = new Copier();
-    build();
     gatherComponents();
+    buildDependencies();
     addBehavior();
-
-    itemBuilder = new DaySummaryItemBuilder(
-      new  TimeUtil(),
-      new TimeFormatter12Hr(),
-      copier,
-      new HeartBuilder(),
-      tbody);
-
-    totalsBuilder = new DaySummaryTotalsBuilder(
-      new HeartBuilder(), 
-      tbody);
-  };
-
-  var build = function() {
-    jQuery('#DaySummary').html(html);
   };
 
   var gatherComponents = function() {
-    tbody = jQuery('#DaySummary tbody');
+    container = document.getElementById('DaySummary');
+  };
+
+  var buildDependencies = function() {
+    dataBuilder = new ProjectSummaryDataBuilder();
+   
+    itemBuilder = new DaySummaryItemBuilder(
+      new  TimeUtil(),
+      new TimeFormatter12Hr(),
+      new Copier(),
+      new HeartBuilder(),
+      container);
+
+    totalsBuilder = new DaySummaryTotalsBuilder(
+      new HeartBuilder(), 
+      container);
   };
 
   var addBehavior = function() {
     App.dispatcher.subscribe('DATE_CHANGED', onDateChanged);
-    App.dispatcher.subscribe('SPAN_SAVED', onSpanSaved);
-    App.dispatcher.subscribe('SPAN_DELETED', onSpanDeleted);
+    App.dispatcher.subscribe('SPAN_SAVED', onSpansChanged);
+    App.dispatcher.subscribe('SPAN_DELETED', onSpansChanged);
     App.dispatcher.subscribe('USE_DECIMAL_HOURS_CHANGED', updateDisplay);
     App.dispatcher.subscribe('PROJECT_SEGMENTOR_CHANGED', updateDisplay);
     App.dispatcher.subscribe('QUARTER_HOUR_CHANGED', updateDisplay);
@@ -54,12 +46,7 @@ var DaySummary = function() {
     updateDisplay();
   };
 
-  var onSpanSaved = function(data) {
-    spans = data.record.spans;
-    updateDisplay();
-  };
-
-  var onSpanDeleted = function(data) {
+  var onSpansChanged = function(data) {
     spans = data.record.spans;
     updateDisplay();
   };
@@ -67,36 +54,7 @@ var DaySummary = function() {
   var updateDisplay = function() {
     var summaryData = dataBuilder.build(spans);
     applyQuarterHour(summaryData);
-
-    if (Object.keys(summaryData).length)
-      populate(summaryData);
-    else
-      showNoContent();
-  };
-
-  var populate = function(summaryData) {
-    tbody.empty();
-
-    const totalMinutes = Object.entries(summaryData).reduce((total, item) => {
-      return total + item[1].time;
-    }, 0);
-
-    var sortedKeys = Object.keys(summaryData).sort();
-    for (var i = 0; i < sortedKeys.length; i++) {
-      var key = sortedKeys[i];
-      itemBuilder.build(summaryData[key], totalMinutes, spans);
-    }
-
-    totalsBuilder.build(summaryData);
-
-  };
-
-  var showNoContent = function() {
-    // TODO: it's shite
-    tbody.empty();
-    const tr = jQuery('<tr>').appendTo(tbody);
-    const td = jQuery('<td colspan="5">').appendTo(tr);
-    td.text('nope');
+    populate(summaryData);
   };
 
   var applyQuarterHour = function(summaryData) {
@@ -111,6 +69,30 @@ var DaySummary = function() {
       projNumbers.roundDelta = roundedMinutes - rawMinutes;
     });
   };
+
+  var populate = function(summaryData) {
+    empty();
+
+    const totalMinutes = calcTotalMinutes(summaryData);
+
+    var sortedKeys = Object.keys(summaryData).sort();
+
+    sortedKeys.forEach(key => itemBuilder.build(summaryData[key], totalMinutes, spans));
+
+    totalsBuilder.build(summaryData);
+  };
+
+  var empty = function () {
+    while (container.lastChild) {
+      container.removeChild(container.lastChild);
+    }
+  };
+
+  var calcTotalMinutes = function(summaryData) {
+    return Object.entries(summaryData).reduce((total, item) => {
+      return total + item[1].time;
+    }, 0);
+ };
 
   init();
 
--- docroot/modules/DaySummary/DaySummaryItemBuilder.js
+++ docroot/modules/DaySummary/DaySummaryItemBuilder.js
@@ -3,107 +3,111 @@ var DaySummaryItemBuilder = function(
   timeFormatter,
   copier, 
   heartBuilder,
-  tbody) {
+  topContainer) {
 
   var build = function(project, totalMinutes, spans) {
-    if (App.globalSettings.quarter_hour)
+    // if (App.globalSettings.quarter_hour)
       buildRounded(project, totalMinutes, spans);
-    else
-      buildRaw(project, totalMinutes, spans);
-  };
-
-  var buildRaw = function(project, totalMinutes, spans) {
-    const tr = jQuery('<tr>').appendTo(tbody);
-    buildProjectLabel(tr, project.label);
-    buildHours(tr, project.rawMinutes);
-    buildHearts(tr, project.rawMinutes); 
-    buildWeight(tr, project.rawMinutes, totalMinutes, project.label);
-    buildMoreToggle(tr);
-    
-    // buildTasks(secondaryTd, project.tasks);
+    // else
+    //   buildRaw(project, totalMinutes, spans);
   };
 
   var buildRounded = function(project, totalMinutes, spans) {
-    var primaryTr = jQuery('<tr>').appendTo(tbody);
-    var secondaryTr = jQuery('<tr>').appendTo(tbody);
-    var secondaryTd = jQuery('<td colspan="5" class="tasks">').appendTo(secondaryTr);
+    const container = document.createElement('day-item');
+    topContainer.append(container);
 
-    buildProjectLabel(primaryTr, project.label);
-    buildHours(primaryTr, project.time);
-    buildDelta(primaryTr, project);
-    buildHearts(primaryTr, project.time); 
-    buildWeight(primaryTr, project.time, totalMinutes, project.label);
-    
-    buildTasks(secondaryTd, project.tasks);
+    container.innerHTML = `
+      <header>
+        <label></label>
+        <weight>
+          <outer><inner></inner></outer>
+          <percent-text>
+        </weight>
+      </header>
+      <time>
+        <hours>${hours(project.time)} <unit>hr</unit></hours>
+        <i class="far fa-copy copy"></i>
+        <delta>${delta(project)}</delta>
+        <heart-container></heart-container>
+      </time>
+      <tasks-container>
+        <ul></ul>
+        <i class="far fa-copy copy"></i>
+      </tasks-container>
+    `;
+
+    container.querySelector('label').appendChild(label(project.label));
+
+    container.querySelector('heart-container').appendChild(
+      heartBuilder.build(project.time/60));
+
+    buildWeight(
+      container,
+      project.time, 
+      totalMinutes, 
+      project.label);
+
+    buildTasks(container.querySelector('ul'), project.tasks);
+
+    addBehavior(container, project);
   };
 
-  var buildProjectLabel = function(tr, label) {
-    const td = jQuery('<td class="project">').appendTo(tr);
-    td.append(App.projectSegmentor.getFormatted(label));
+  var label = function(label) {
+    return App.projectSegmentor.getFormatted(label)[0];
   };
 
-  var buildHours = function(tr, minutes) {
-    const hours = timeUtil.formatTime(minutes);
-    const td = jQuery('<td class="hours">')
-      .appendTo(tr)
-      .text(hours);
-    jQuery('<span class="unit"> hr</span>').appendTo(td);
-    jQuery('<i class="far fa-copy copy">')
-      .data('copy', hours)
-      .appendTo(td)
-      .click(copy)
+  var hours = function(minutes) {
+    return timeUtil.formatTime(minutes);
   };
 
-  var buildDelta = function(tr, project) {
+  var delta = function(project) {
     const sign = project.roundDelta < 0 ? '-' : '+';
     const delta = timeUtil.formatTime(Math.abs(project.roundDelta));
-    jQuery('<td class="delta">').appendTo(tr).text(sign + delta);
+    return sign + delta;
   };
 
-  var buildHearts = function(tr, minutes) {
-    const td = jQuery('<td class="hearts">').appendTo(tr);
-    td.append(heartBuilder.build(minutes/60));
-  };
-
-  var buildWeight = function(tr, minutes, totalMinutes, label) {
-    const td = jQuery('<td class="weight">').appendTo(tr);
-
-    const outer = jQuery('<outer>').appendTo(td);
-    const inner = jQuery('<inner>').appendTo(outer);
-    const percentEl = jQuery('<percent-text>').appendTo(td);
-
+  var buildWeight = function(container, minutes, totalMinutes, label) {
     const percent = Math.round(minutes*100/totalMinutes);
-    inner.width(percent);
-    percentEl.text(percent + '%');
-
     const projectRoot = App.projectSegmentor.segment(label)[0];
-    inner.css('background-color', App.colorGenerator.generate(projectRoot, 0.8));
+    const color = App.colorGenerator.generate(projectRoot, 0.8);
+    
+    container.querySelector('percent-text').textContent = percent + '%';
+    
+    const inner = container.querySelector('inner');
+    inner.style.width = percent + 'px';
+    inner.style.backgroundColor = color;
   };
 
-  var buildTasks = function(td, tasks) {
-    const listEl = jQuery('<ul>').appendTo(td);
-    jQuery.each(tasks, function(index, task) {
-      jQuery('<li>')
-        .appendTo(listEl)
-        .text(task);
+  var buildTasks = function(list, tasks) {
+    tasks.forEach(task => {
+      const item = document.createElement('li')
+      item.textContent = task;
+      list.appendChild(item);
     });
+  };
 
-    jQuery('<i class="far fa-copy copy">')
-      .data('copy', tasks.join('\n'))
-      .appendTo(td)
-      .click(copy)
+  var addBehavior = function(container, project) {
+    const hourCopy = container.querySelector('time i');
+    hourCopy.setAttribute('copy', timeUtil.formatTime(project.time));
+    hourCopy.addEventListener('click', copy);
+
+    const taskCopy = container.querySelector('tasks-container i');
+    taskCopy.setAttribute('copy', project.tasks.join('\n'));
+    taskCopy.addEventListener('click', copy);
   };
 
   var copy = function() {
-    var button = jQuery(this);
-    var text = button.data('copy');
+    const val = this.getAttribute('copy');
 
-    button.removeClass('fa-copy').addClass('fa-check');
-    setTimeout(function() {
-      button.removeClass('fa-check').addClass('fa-copy');
+    this.classList.remove('fa-copy');
+    this.classList.add('fa-check');
+
+    setTimeout(() => {
+      this.classList.remove('fa-check');
+      this.classList.add('fa-copy');
     }, 400);
 
-    copier.copy(text);
+    copier.copy(val);
   };
 
   return {
